7250bbd9ec98c06a6f1268912a7c50cfd171410a
Павел Гапоненко <p.gaponenko@worksolutions.ru>
2024-04-04 11:57:57 +0300

    Рефакторинг SecurityConfig

--- src/main/java/com/ws/springkeycloakdemo/config/SecurityConfig.java
+++ src/main/java/com/ws/springkeycloakdemo/config/SecurityConfig.java
@@ -12,79 +12,54 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.stream.Stream;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-            .oauth2Login(Customizer.withDefaults()).logout(logout -> logout
-                .logoutSuccessUrl("/")
-                .logoutUrl("/logout")
-                .invalidateHttpSession(true) // Очистка HTTP сессии после logout
-                .deleteCookies("JSESSIONID", "SESSION") // Удаление cookies после logout
-                .clearAuthentication(true)
+            .oauth2ResourceServer(
+                oauth2 -> oauth2.jwt(Customizer.withDefaults())
             )
+            .oauth2Login(Customizer.withDefaults())
             .authorizeHttpRequests(
                 customizer -> customizer
-                    .requestMatchers("/error").permitAll()
                     .requestMatchers("/manager").hasRole("MANAGER")
                     .anyRequest().authenticated()
             )
             .build();
     }
 
-    /**
-     * Выбираем значение строки scope, выбираем значения для прав
-     */
     @Bean
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        var converter = new JwtAuthenticationConverter();
+        converter.setPrincipalClaimName("preferred_username");
 
-        // указываем откуда брать имя пользователя
-        jwtAuthenticationConverter.setPrincipalClaimName("preferred_username");
-
-        var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-
-        // конвертер для прав
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
-            var authorities = jwtGrantedAuthoritiesConverter.convert(jwt);
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
             var roles = jwt.getClaimAsStringList("demo_roles");
 
-            return Stream.concat(
-                authorities.stream(),
-                roles.stream()
-                    .filter(role -> role.startsWith("ROLE_"))
-                    .map(SimpleGrantedAuthority::new)
-                    .map(GrantedAuthority.class::cast))
+            return roles.stream()
+                .filter(role -> role.startsWith("ROLE_"))
+                .map(SimpleGrantedAuthority::new)
+                .map(GrantedAuthority.class::cast)
                 .toList();
         });
 
-        return jwtAuthenticationConverter;
+        return converter;
     }
 
-    /**
-     * OidcUserRequest - запрос к Keycloak
-     * OidcUser - возвращенный
-     */
     @Bean
     public OAuth2UserService<OidcUserRequest, OidcUser> oAuth2UserService() {
         var oidcUserService = new OidcUserService();
         return userRequest -> {
             var oidcUser = oidcUserService.loadUser(userRequest);
             var roles = oidcUser.getClaimAsStringList("demo_roles");
-            var authorities = Stream.concat(
-                oidcUser.getAuthorities().stream(),
-                roles.stream()
-                    .filter(role -> role.startsWith("ROLE_"))
-                    .map(SimpleGrantedAuthority::new)
-                    .map(GrantedAuthority.class::cast)
-                )
+
+            var authorities = roles.stream()
+                .filter(role -> role.startsWith("ROLE_"))
+                .map(SimpleGrantedAuthority::new)
+                .map(GrantedAuthority.class::cast)
                 .toList();
 
             return new DefaultOidcUser(authorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
